8197e282acd5aa3f3837d4eee680752d5f2b382f
insub2004 <insub2004@naver.com>
2025-08-18 07:12:37 +0900

    step2-1

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -21,26 +21,26 @@ public class SharedCounter {
 
     public SharedCounter(long count) {
         //TODO#1-1 생성자를 초기화 합니다. count < 0 IllegalArgumentException아 발생 합니다.
-
+        if (count < 0) {
+            throw new IllegalArgumentException();
+        }
         this.count = 0l;
-
     }
 
     public long getCount(){
         //TODO#1-2 count 를 반환 합니다.
-
-        return 0l;
+        return count;
     }
 
     public long increaseAndGet(){
         //TODO#1-3 count = count + 1 증가시키고 count를 반환 합니다.
-
-        return 0l;
+        count = count + 1;
+        return count;
     }
 
     public long decreaseAndGet(){
         //TODO#1-4 count = count-1 감소시키고 count를 반환 합니다.
-
-        return 0l;
+        count = count - 1;
+        return count;
     }
 }
